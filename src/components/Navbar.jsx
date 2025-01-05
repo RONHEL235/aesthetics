6ed3376510880682c39578dfddb42f531fc622dc
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-05 20:38:19 +0200

    add toggle functionality to Navbar component and update structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
   return (
-    <div>Navbar</div>
+    <nav className="border-b-2">
+        <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
+            
+        </div>
+    </nav>
   )
-}
+ className="border-b-2"
+
 
 export default Navbar
