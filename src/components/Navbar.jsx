a55b91c4a7d18ec313ede1140824ba1fe0b60f58
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-07 23:40:57 +0200

    fix Navbar component structure and improve accessibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,17 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
           <div className="pl-2">
             <a href="#">
-              <img src={logo} width={150} height={15} alt="Aesthetics"></img>
+              <img src={logo} width={150} height={15} alt="Aesthetics" />
             </a>
+          </div>
+
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-2xl pr-2 focus:outline-none" arial-label={isOpen? "Close Menu" : "Open Menu"}>
+              
+            </button>
           </div> 
         </div>
-    </nav>
+    </nav
   )
  className="border-b-2"
 
