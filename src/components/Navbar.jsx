5ae33fba22f8fab817beb2322b562175b2b4e8c0
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-07 23:35:11 +0200

    add logo to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,11 @@ const Navbar = () => {
   return (
     <nav className="border-b-2">
         <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
-            
+          <div className="pl-2">
+            <a href="#">
+              <img src={logo} width={150} height={15} alt="Aesthetics"></img>
+            </a>
+          </div> 
         </div>
     </nav>
   )
