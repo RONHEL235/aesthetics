954ff2021a6491e6f63c06a375eb8c6da13375a2
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-09 22:05:42 +0200

    add Services component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Services from "./components/Services"
 
 const App = () => {
   return (
     <main className="overflow-x-hidden antialiased text-neutral-900">
       <Navbar />
       <Hero />
+      <Services />
     </main> 
   )
 }
