5d9f2ae6db5cbc73e3b9340a8cde51a94ebdae3f
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-01-09 22:22:17 +0200

    add layout and content structure to Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,23 @@
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-w-7xl mx-auto border-b-2" id="services">
+      <div className="my-20">
+        <h2 className="text-xl lg:text-3xl tracking-tighter text-center uppercase mb-20">
+          This what I like
+        </h2>
+        {SERVICES_CONTENT.map((service, index) => (
+          <div key={index} className="mb-12 mx-4 flex flex-col lg:flex-row">
+            <div className={`lg:w-1/2 mb-4 lg:mb-0 ${index % 2 === 0? "": "lg:order-2"}`}>
+              <img src={service.image} alt={service.title} className="w-full h-auto object-cover rounded-lg" />
+            </div>
+
+            <div className={`lg:w-1/2`}>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
